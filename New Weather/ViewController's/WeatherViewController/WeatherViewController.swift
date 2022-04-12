1dc4954f04690581268442717784a88a0c032ae7
AlexShi <alexshihov55@gmail.com>
2022-04-12 16:28:09 +0300

    add StackView
    StackView with Buttons by the day's of week

--- New Weather/ViewController's/WeatherViewController/WeatherViewController.swift
+++ New Weather/ViewController's/WeatherViewController/WeatherViewController.swift
@@ -10,18 +10,61 @@ import UIKit
 
 @IBDesignable class WeatherViewController: UIViewController {
 
+    //MARK: - Properties
+    
+    @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var allButtons = [UIButton]()
+    var selectDay: Days? = nil {
+        didSet {
+            self.updateSelectedDay()
+        }
+    }
+//MARK: - viewDidLoad
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(UINib(nibName: "WeatherCell", bundle: nil), forCellWithReuseIdentifier: "reuse")
         
+        setupButtons()
+        
+        stackView.spacing = 8
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        }
+    
+    //MARK: - Method's
+    
+    @objc private func selectDay(_ sender: UIButton) {
+        
+        guard let index = self.allButtons.firstIndex(of: sender) else { return }
+        guard let day = Days(rawValue: index) else { return }
+        selectDay = day
+        }
+    
+    private func updateSelectedDay() {
+        for (index, button) in self.allButtons.enumerated() {
+            guard let day = Days(rawValue: index) else { continue }
+            button.isSelected = day == self.selectDay
+        }
     }
     
-
-
+    private func setupButtons() {
+        for day in Days.allDays {
+            let button = UIButton(type: .system)
+            
+            button.setTitle(day.title ,for: .normal)
+            button.setTitleColor(.blue, for: .normal)
+            button.setTitleColor(.darkGray, for: .selected)
+            button.addTarget(self, action: #selector(selectDay( _:)), for: .touchUpInside)
+            
+            allButtons.append(button)
+            stackView.addArrangedSubview(button)
+        }
+    }
 }
