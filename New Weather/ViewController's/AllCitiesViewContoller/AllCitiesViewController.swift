109926dd3dc3a6e23551e2b4d947d2b833b718b4
AlexShi <alexshihov55@gmail.com>
2022-04-13 14:01:00 +0300

    add gradient at view's with tableVC

--- New Weather/ViewController's/AllCitiesViewContoller/AllCitiesViewController.swift
+++ New Weather/ViewController's/AllCitiesViewContoller/AllCitiesViewController.swift
@@ -12,15 +12,31 @@ class AllCitiesViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var gradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: citiesCellIdentifier)
-        
-        
-        
+        }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        if gradientLayer.superlayer != nil {
+            gradientLayer.removeFromSuperlayer()
+        }
+        let topColor = UIColor(red: 0.00, green: 0.99, blue: 1.00, alpha: 1.00)
+        let bottomColor = UIColor.systemIndigo
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.frame = tableView.bounds
+        let backgroundView = UIView(frame: tableView.bounds)
+        backgroundView.layer.insertSublayer(gradientLayer, at: 0)
+        tableView.backgroundView = backgroundView
     }
     
 
--- New Weather/ViewController's/MyCitiesViewController/MyCitiesViewController.swift
+++ New Weather/ViewController's/MyCitiesViewController/MyCitiesViewController.swift
@@ -28,6 +28,22 @@ class MyCitiesViewController: UIViewController {
         
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        if gradientLayer.superlayer != nil {
+            gradientLayer.removeFromSuperlayer()
+        }
+        let topColor = UIColor(red: 0.00, green: 0.99, blue: 1.00, alpha: 1.00)
+        let bottomColor = UIColor.systemIndigo
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.frame = tableView.bounds
+        let backgroundView = UIView(frame: tableView.bounds)
+        backgroundView.layer.insertSublayer(gradientLayer, at: 0)
+        tableView.backgroundView = backgroundView
+    }
     
     
     //MARK: - Selector
